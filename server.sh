851f267b3eb5bc3d55a4ee481a64cadc555b9e35
NinoFares <bouzid.faresabn@gmail.com>
2020-12-08 12:34:56 +0100

    update server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ./cli-md-ui && ng serve &
-gin --port 4201 --path . --build ./cli-md-backend/src/ --i --all &
+ng serve &
+gin --port 4201 --path . --build ../cli-md-backend/src/main --i --all &
 
 wait
